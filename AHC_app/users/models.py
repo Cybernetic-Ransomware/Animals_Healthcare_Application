102aed7c1219c91f43297e8e243039a29cb5b983
Cybernetic-Ransomware <aleksander.marszalkiewicz.pp@gmail.com>
2023-07-06 10:12:17 +0200

    Login and user management

--- AHC_app/users/models.py
+++ AHC_app/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    registration_date = models.DateField(auto_now_add=True)
+    image = models.ImageField(default='profile_pics/signup.png', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'Profile of {self.user.username}'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if any([img.height > 300, img.width > 300]):
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- AHC_app/users/urls.py
+++ AHC_app/users/urls.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, \
+    PasswordResetConfirmView, PasswordResetCompleteView
 
-from .views import LoginView
+from . import views as user_views
 
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    # path('register/', user_views.register, name='register'),
-    # path('profile/', user_views.profile, name='profile'),
+    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('register/', user_views.register, name='register'),
+    path('login_success/', auth_views.LoginView.as_view(template_name='users/login_success.html'), name='login_success'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
+
+    path('password-reset/', PasswordResetView.as_view(
+        template_name='users/password_reset.html',
+        html_email_template_name='users/password_reset_email.html'),
+         name='password-reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+
 ]
+
