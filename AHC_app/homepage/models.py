ba0feadbfb07a70f49ac812a4661b5a0edbe6a5f
Cybernetic-Ransomware <71835339+Cybernetic-Ransomware@users.noreply.github.com>
2023-07-10 00:17:39 +0200

    testing examples

--- AHC_app/homepage/models.py
+++ AHC_app/homepage/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class AnimalTitle(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.ImageField(default='AHC_app/static/media/icons/chinchilla.png',
+                                upload_to='AHC_app/static/media/animal_pic')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('article-detail', kwargs={'pk': self.pk})
---
+++ AHC_app/homepage/tests/models/test_animal_title.py
@@ -0,0 +1,3 @@
+class TestAnimalTitle:
+    def test_should_create_proper_object_when_information_given(self):
+        pass
