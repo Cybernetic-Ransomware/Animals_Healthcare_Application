3b039ca6ce9849b00ceb3aacbd97e6fa4f2389ca
Cybernetic-Ransomware <aleksander.marszalkiewicz.pp@gmail.com>
2023-07-27 14:12:07 +0200

    Prepare animals models

--- AHC_app/animals/models.py
+++ AHC_app/animals/models.py
@@ -1,5 +1,56 @@
 from django.db import models
 
 
+class BiometricCustomRecords(models.Model):
+    biometric_record = None
+    record_name = None
+    record_value = None
+
+
+class BiometricRecord(models.Model):
+    animal = None
+    height = None
+    weight = None
+    custom_values = None
+
+
+class MedicalRecord(models.Model):
+    animal = None
+
+    date_creation = None
+    date_updated = None
+    date_event_started = None
+    date_event_ended = None
+
+    participants = None
+    place = None
+
+    short_description = None
+    full_description = None
+
+    type_of_event = None
+    event_details = None
+
+
 class Animal(models.Model):
-    pass
+    full_name = None
+    short_description = None
+    long_description = None
+
+    birthdate = None
+    profile_image = None
+    creation_date = None
+
+    allowed_keepers = None
+    first_contact_vet = None
+    first_contact_medical_place = None
+
+    biometric_records = None
+    biometric_records_history = None
+
+    last_control_visit = None
+    medical_records = None
+
+    feeding_records = None
+    current_diet = None
+    current_medications = None
