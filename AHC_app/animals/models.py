a2c43c85abbefbe3922c3845ffb00f885f5cc3ac
Cybernetic-Ransomware <71835339+Cybernetic-Ransomware@users.noreply.github.com>
2023-08-07 23:45:08 +0200

    Main animal's profile template

--- AHC_app/animals/models.py
+++ AHC_app/animals/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from users.models import Profile as UserProfile
 
@@ -5,13 +7,13 @@ from users.models import Profile as UserProfile
 class BiometricRecord(models.Model):
     height = models.IntegerField(
         default=0
-    )  # always in grams, set validation to int values (if float, ask if save as integer grams)
+    )  # always in grams, set validation to int values (if is float, ask if save as integer grams)
     height_unit_to_present = models.CharField(max_length=3, default="g", blank=False)
     height_date_updated = models.DateTimeField(auto_now_add=True, editable=True)
 
     weight = models.IntegerField(
         default=0
-    )  # always in mm, set validation to int values (if float, ask if save as integer grams)
+    )  # always in mm, set validation to int values (if is float, ask if save as integer grams)
     weight_unit_to_presen = models.CharField(max_length=3, default="mm", blank=False)
     weight_date_updated = models.DateTimeField(auto_now_add=True, editable=True)
 
@@ -41,7 +43,9 @@ class Animal(models.Model):
     )  # dodac okresową notyfikację o braku ownera - przypisuje admin z panelu
     allowed_users = models.ManyToManyField(UserProfile, null=True)
 
+    first_contact_vet = models.CharField(max_length=250, blank=True)
     # first_contact_vet = models.ForeignKey(Vet_pofile)
+    first_contact_medical_place = models.CharField(max_length=250, blank=True)
     # first_contact_medical_place = models.ForeignKey(Place_profile)
 
     last_control_visit = models.DateTimeField(null=True, blank=True)
@@ -57,47 +61,61 @@ class Animal(models.Model):
 
 
 class CurrentDiet(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     animal = models.ForeignKey(Animal, on_delete=models.SET_NULL)
-    start_date = None
-    end_date = None
-    food_type = None
-    name = None
-    producer = None
-    description = None
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    food_type = models.CharField(max_length=50)
+    name = models.CharField(max_length=80)
+    producer = models.CharField(max_length=120)
+    description = models.CharField(max_length=250)
 
-    # wykorzystywana zarówno do jedzenia, jak i do medykamentów,
     # + sygnał przepisywania na notatkę (obsłuż haszowanie do edytowania id notatki i rodziel daty zakładane od rzeczywistych)
     # + możliwość kopiowania do innego zwierzęcia
     # później stworzyć apkę do kartoteki produktów, zbudować rejestrację i agregację kosztów
 
 
-class CurrentMedicine(CurrentDiet):
-    frequency_description = None
+class CurrentMedicine(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    animal = models.ForeignKey(Animal, on_delete=models.SET_NULL)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    food_type = models.CharField(max_length=50)
+    name = models.CharField(max_length=80)
+    producer = models.CharField(max_length=120)
+    description = models.CharField(max_length=250)
 
-    notifications_is_active = None
-    notification_form = None
-    notification_message = None
-    notification_frequency_interval = None
+    frequency_description = models.CharField(max_length=250)
 
-    # rodzielić od karmy (nie dziedziczyć)
+    notifications_is_active = models.BooleanField(default=False, null=False)
+    notification_form = models.CharField(max_length=50)
+    notification_message = models.CharField(max_length=2500)
+    notification_frequency_interval = models.DurationField(null=True, blank=True)
+
     # co jeszcze odroznia medykament od pokarmu?
     # dostępne inne produkty z kartoteki
     # zaplanować pod możliwość konwertowania datetime i ustawienia notyfikacji
 
 
 class MedicalRecord(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
 
-    date_creation = None
-    date_updated = None
-    date_event_started = None
-    date_event_ended = None
+    date_creation = models.DateTimeField(auto_now_add=True, editable=False)
+    date_updated = models.DateTimeField(auto_now=True, editable=True)
+    date_event_started = models.DateField(null=True, blank=True)
+    date_event_ended = models.DateField(null=True, blank=True)
 
-    participants = None
-    place = None
+    # to cahnge to a new app models
+    participants = models.CharField(max_length=80)
+    place = models.CharField(max_length=80)
 
-    short_description = None
-    full_description = None
+    short_description = models.CharField(max_length=125)
+    full_description = models.CharField(max_length=2500)
 
-    type_of_event = None
+    type_of_event = models.CharField(max_length=50)
+    # reference to a new table, if needed for some types of records
     event_details = None
+
+    # to use for hashtags
+    note_tags = None
