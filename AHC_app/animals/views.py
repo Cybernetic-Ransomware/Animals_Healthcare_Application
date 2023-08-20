7ca9e2f4e7d32bcaba38c722fd4d30b9652a41dd
Cybernetic-Ransomware <71835339+Cybernetic-Ransomware@users.noreply.github.com>
2023-08-21 01:33:31 +0200

    Views fixing: redirection and query

--- AHC_app/animals/views.py
+++ AHC_app/animals/views.py
@@ -1,12 +1,12 @@
 import uuid
 
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse, HttpResponseNotAllowed
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import FormView
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadRequest, HttpResponseNotAllowed
-from django.shortcuts import render, redirect
-from django.urls import reverse
 
 from .forms import AnimalRegisterForm, ImageUploadForm
 from .models import Animal
@@ -15,13 +15,13 @@ from .models import Animal
 @login_required
 # @permission_required(True, login_url='homepage')  # napisz własną funkcję weryfikującym właściciela z ID zweirzęcia
 def profile(request, h_pk):
-    if request.method == 'GET':
+    if request.method == "GET":
         animal_id = uuid.UUID(h_pk)
         animal = Animal.objects.get(pk=animal_id)
     else:
         return HttpResponseNotAllowed("405 Method Not Allowed")
 
-    return render(request, 'animals/profile.html', {'animal': animal})
+    return render(request, "animals/profile.html", {"animal": animal})
 
 
 class AnimalProfileDetailView(LoginRequiredMixin, DetailView):
@@ -30,22 +30,24 @@ class AnimalProfileDetailView(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['animal'] = self.object
-        context['name'] = self.object.owner
-        context['image'] = self.object.profile_image.url
-        context['upload_image_url'] = reverse('upload_image', kwargs={'pk': self.object.id})
+        context["animal"] = self.object
+        context["name"] = self.object.owner
+        context["image"] = self.object.profile_image.url
+        context["upload_image_url"] = reverse(
+            "upload_image", kwargs={"pk": self.object.id}
+        )
         return context
 
 
 @login_required
 # @permission_required(True, login_url='homepage')  # napisz własną funkcję weryfikującym właściciela z ID zweirzęcia
 def stable(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         return HttpResponse("501 Not Implemented: site in build")
     else:
         return HttpResponse("501 Not Implemented: site in build")
 
-    return render(request, 'animals/profile.html', {})
+    # return render(request, 'animals/profile.html', {})
 
 
 class CreateFormView(LoginRequiredMixin, FormView):
@@ -58,24 +60,24 @@ class CreateFormView(LoginRequiredMixin, FormView):
         new_animal.owner = self.request.user.profile
         new_animal.save()
 
-        self.success_url = reverse('animal_profile', kwargs={'h_pk': new_animal.id})
+        self.success_url = reverse("animal_profile", kwargs={"pk": new_animal.id})
 
         return super().form_valid(form)
 
 
 class ImageUploadView(LoginRequiredMixin, FormView):
-    template_name = 'animals/image.html'
+    template_name = "animals/image.html"
     form_class = ImageUploadForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['animal_id'] = self.kwargs['pk']
+        context["animal_id"] = self.kwargs["pk"]
         return context
 
     def form_valid(self, form):
         form.save()
 
-        animal_id = self.kwargs['pk']
+        animal_id = self.kwargs["pk"]
 
-        self.success_url = reverse('animal_profile', kwargs={'pk': animal_id})
+        self.success_url = reverse("animal_profile", kwargs={"pk": animal_id})
         return redirect(self.success_url)
--- AHC_app/homepage/views.py
+++ AHC_app/homepage/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from animals.models import Animal
+from django.db.models import Q
 from django.views.generic import TemplateView
 
 
 class HomepageView(TemplateView):
     template_name = "homepage/homepage.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # recent_animals = Animal.objects.order_by('-creation_date').values('id', 'full_name', 'profile_image')[:3]
+        # recent_animals = Animal.objects.order_by('-creation_date').values('id', 'full_name', 'profile_image__url')[:3]
+
+        query = Animal.objects.filter(
+            Q(owner=self.request.user.profile)
+            | Q(allowed_users=self.request.user.profile)
+        ).order_by("-creation_date")
+
+        context["recent_animals"] = query[:3]
+        context["example_animal_id"] = query.latest("creation_date").id
+
+        return context
