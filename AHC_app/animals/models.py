70cdf33ee2398c223b003a1890b7534d4ad79d50
Cybernetic-Ransomware <aleksander.marszalkiewicz.pp@gmail.com>
2023-09-11 09:25:01 +0200

    start a new app

--- AHC_app/animals/models.py
+++ AHC_app/animals/models.py
@@ -95,27 +95,3 @@ class CurrentMedicine(models.Model):
     # co odroznia medykament od pokarmu?
     # dostępne inne produkty z kartoteki
     # zaplanować pod możliwość konwertowania datetime i ustawienia notyfikacji
-
-
-class MedicalRecord(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
-
-    date_creation = models.DateTimeField(auto_now_add=True, editable=False)
-    date_updated = models.DateTimeField(auto_now=True, editable=True)
-    date_event_started = models.DateField(null=True, blank=True)
-    date_event_ended = models.DateField(null=True, blank=True)
-
-    # to cahnge to a new app models
-    participants = models.CharField(max_length=80)
-    place = models.CharField(max_length=80)
-
-    short_description = models.CharField(max_length=125)
-    full_description = models.CharField(max_length=2500)
-
-    type_of_event = models.CharField(max_length=50)
-    # reference to a new table, if needed for some types of records
-    event_details = None
-
-    # to use for hashtags
-    note_tags = None
