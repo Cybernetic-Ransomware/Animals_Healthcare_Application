f0bd502e11125a020674bcfd457a72c1dab64577
Cybernetic-Ransomware <aleksander.marszalkiewicz.pp@gmail.com>
2023-09-12 15:21:55 +0200

    Form endpoints

--- AHC_app/medical_notes/forms.py
+++ AHC_app/medical_notes/forms.py
@@ -6,30 +6,33 @@ from animals.models import Animal as AnimalProfile
 from .models import MedicalRecord
 
 
-class MedicalRecordForm(forms.ModelForm):  # or forms.Form
+class MedicalRecordForm(forms.ModelForm):
     class Meta:
         model = MedicalRecord
         fields = [
-            'date_event_started',
-            'date_event_ended',
+            'type_of_event',
             'participants',
             'place',
             'short_description',
             'full_description',
-            'type_of_event',
+            'date_event_started',
+            'date_event_ended'
+
         ]
 
-        # TYPES_OF_EVENTS = (
-        #     ('option1', 'Opcja 1'),
-        #     ('option2', 'Opcja 2'),
-        #     ('option3', 'Opcja 3'),
-        # )
-
+        TYPES_OF_EVENTS = (
+            ('medical_visit',     'Medical visit'),
+            ('biometric_record',  'Biometric record'),
+            ('diet_note',         'Diet note'),
+            ('medicament_note',   'Medicament note'),
+            ('other_user_note',   'Other'),
+        )
         widgets = {
-            "date_event_started": forms.DateInput(attrs={"type": "date"}),
-            "date_event_ended": forms.DateInput(attrs={"type": "date"}),
+            "date_event_started": forms.DateInput(attrs={"type": "date", "required": False}),
+            "date_event_ended": forms.DateInput(attrs={"type": "date", "required": False}),
             "short_description": forms.Textarea(attrs={"rows": 3, "cols": 2}),
-            "full_description": forms.Textarea(attrs={"rows": 12, "cols": 2}),
-            # "full_description": forms.ChoiceField(choices=TYPES_OF_EVENTS)
-            'type_of_event': forms.Select(attrs={'class': 'custom-select'})
+            "full_description": forms.Textarea(attrs={"rows": 12, "cols": 2, "required": False}),
+            "type_of_event": forms.Select(choices=TYPES_OF_EVENTS, attrs={'class': 'custom-select'}),
+            "participants": forms.TextInput(attrs={"required": False}),
+            "place": forms.TextInput(attrs={"required": False})
         }
