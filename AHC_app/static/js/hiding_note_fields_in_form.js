4b7d62d48b4ae1cf80a9d1e8c311f663f6c9c4bf
Cybernetic-Ransomware <aleksander.marszalkiewicz.pp@gmail.com>
2023-09-25 11:36:48 +0200

    Hide form fields also after the first page load

--- AHC_app/static/js/hiding_note_fields_in_form.js
+++ AHC_app/static/js/hiding_note_fields_in_form.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    function handleTypeOfEventChange() {
         const typeOfEventField = document.getElementById('id_type_of_event');
         const participantsField = document.getElementById('div_id_participants');
         const placeField = document.getElementById('div_id_place');
@@ -6,54 +7,23 @@ document.addEventListener('DOMContentLoaded', function() {
         const eventstartedField = document.getElementById('div_id_date_event_started');
         const eventendedField = document.getElementById('div_id_date_event_ended');
 
-        typeOfEventField.addEventListener('change', function() {
-            if (typeOfEventField.value === 'fast_note') {
-                participantsField.style.display = 'none';
-                placeField.style.display = 'none';
-                fulldescriptionField.style.display = 'none';
-                eventstartedField.style.display = 'none';
-                eventendedField.style.display = 'none';
-            }
-            else if (typeOfEventField.value === 'medical_visit') {
-                participantsField.style.display = 'block';
-                placeField.style.display = 'block';
-                fulldescriptionField.style.display = 'block';
-                eventstartedField.style.display = 'block';
-                eventendedField.style.display = 'none';
-            }
-            else if (typeOfEventField.value === 'biometric_record') {
-                participantsField.style.display = 'block';
-                placeField.style.display = 'none';
-                fulldescriptionField.style.display = 'none';
-                eventstartedField.style.display = 'block';
-                eventendedField.style.display = 'none';
-            }
-            else if (typeOfEventField.value === 'diet_note') {
-                participantsField.style.display = 'none';
-                placeField.style.display = 'none';
-                fulldescriptionField.style.display = 'block';
-                eventstartedField.style.display = 'block';
-                eventendedField.style.display = 'block';
-            }
-            else if (typeOfEventField.value === 'medicament_note') {
-                participantsField.style.display = 'none';
-                placeField.style.display = 'none';
-                fulldescriptionField.style.display = 'block';
-                eventstartedField.style.display = 'block';
-                eventendedField.style.display = 'block';
-            }
-            else if (typeOfEventField.value === 'other_user_note') {
-                participantsField.style.display = 'block';
-                placeField.style.display = 'block';
-                fulldescriptionField.style.display = 'block';
-                eventstartedField.style.display = 'block';
-                eventendedField.style.display = 'block';
-            }else {
-                participantsField.style.display = 'none';
-                placeField.style.display = 'none';
-                fulldescriptionField.style.display = 'none';
-                eventstartedField.style.display = 'none';
-                eventendedField.style.display = 'none';
-            }
-        });
-    });
+        if (typeOfEventField.value === 'fast_note') {
+            participantsField.style.display = 'none';
+            placeField.style.display = 'none';
+            fulldescriptionField.style.display = 'none';
+            eventstartedField.style.display = 'none';
+            eventendedField.style.display = 'none';
+        } else if (typeOfEventField.value === 'medical_visit') {
+            participantsField.style.display = 'block';
+            placeField.style.display = 'block';
+            fulldescriptionField.style.display = 'block';
+            eventstartedField.style.display = 'block';
+            eventendedField.style.display = 'none';
+        }
+    }
+
+    handleTypeOfEventChange();
+
+    const typeOfEventField = document.getElementById('id_type_of_event');
+    typeOfEventField.addEventListener('change', handleTypeOfEventChange);
+});
