bf2187e6271e12e712ba6d35d2ec20941e9f0b1e
Cybernetic-Ransomware <aleksander.marszalkiewicz.pp@gmail.com>
2023-09-27 13:20:16 +0200

    Migration note types to second app, implement all notes paginated view

--- AHC_app/medical_notes/urls.py
+++ AHC_app/medical_notes/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('<uuid:pk>/create/', notes_views.CreateNoteFormView.as_view(), name='note_create'),
     # path('<uuid:pk>/delete/', notes_views.AnimalDeleteView.as_view(), name='note_delete'),
 
+    path('<uuid:pk>/notes/', notes_views.FullTimelineOfNotes.as_view(), name='full_timeline_of_notes'),
+
+
 ]
--- AHC_app/medical_notes/views.py
+++ AHC_app/medical_notes/views.py
@@ -3,10 +3,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.generic.edit import FormView
+from django.views.generic.list import ListView
 
 from .forms import MedicalRecordForm
+from .models import MedicalRecord
 
 
+# change animal into ManyToMany (animal -> animals), append create field for other animals, hashtags,
 class CreateNoteFormView(LoginRequiredMixin, UserPassesTestMixin, FormView):
     template_name = "medical_notes/create.html"
     form_class = MedicalRecordForm
@@ -38,3 +41,32 @@ class CreateNoteFormView(LoginRequiredMixin, UserPassesTestMixin, FormView):
     def get_success_url(self):
         animal_id = self.kwargs.get("pk")
         return reverse("animal_profile", kwargs={"pk": animal_id})
+
+
+class FullTimelineOfNotes(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    model = MedicalRecord
+    template_name = 'medical_notes/full_timeline_of_notes.html'
+    context_object_name = 'notes'
+    ordering = ['-date_creation']
+    paginate_by = 4
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        animal_id = self.kwargs.get("pk")
+        animal = get_object_or_404(AnimalProfile, id=animal_id)
+        query = MedicalRecord.objects.filter(animal=animal)
+
+        context["notes"] = query
+        return context
+
+    def test_func(self):
+        user = self.request.user.profile
+
+        animal_id = self.kwargs.get("pk")
+        animal = get_object_or_404(AnimalProfile, id=animal_id)
+
+        all_users = set(animal.allowed_users.all())
+        all_users.add(animal.owner)
+
+        return user in all_users
