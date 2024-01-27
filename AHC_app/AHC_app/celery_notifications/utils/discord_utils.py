24ccd21a3bec9752a1d889830cedc6906fc6b543
Cybernetic-Ransomware <71835339+Cybernetic-Ransomware@users.noreply.github.com>
2024-01-27 07:29:37 +0100

    Fix: discord bot task

--- AHC_app/AHC_app/celery_notifications/utils/discord_utils.py
+++ AHC_app/AHC_app/celery_notifications/utils/discord_utils.py
@@ -1,35 +1,33 @@
 import asyncio
 
 from discord import Client, Intents
+from discord.ext import commands
 from django.conf import settings
 
 TOKEN = settings.DISCORD_TOKEN
 
 
-def init_bot() -> Client:
-    intents = Intents.default()
-    intents.message_content = True
+def send_via_discord(user_id: int, user_message: str) -> None:
+    def init_bot() -> Client:
+        intents = Intents.default()
+        intents.members = True
+        intents.message_content = True
 
-    client = Client(intents=intents)
+        # discord_client = Client(intents=intents)
+        discord_client = commands.Bot(command_prefix="!", intents=intents)
 
-    return client
+        return discord_client
 
+    client = init_bot()
 
-async def send_via_discord(user_id: int, user_message: str) -> None:
-    bot = init_bot()
-    bot.run(TOKEN)
-
-    @bot.event
+    @client.event
     async def on_ready():
-        user = bot.get_user(user_id)
-        print(user_id)
-
-        if user:
-            await user.send(user_message)
-
-        print(user_message)
+        for guild in client.guilds:
+            user = guild.get_member(user_id)
+            if user:
+                await user.send(user_message)
 
         await asyncio.sleep(2)
-        await bot.close()
+        await client.close()
 
-    await bot.start(TOKEN)
+    client.run(TOKEN)
