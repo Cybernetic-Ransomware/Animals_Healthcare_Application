88443fe0f4c6c6ce2f3fcdf537db7cdad1108b45
Cybernetic-Ransomware <71835339+Cybernetic-Ransomware@users.noreply.github.com>
2024-05-05 21:40:16 +0200

    Redirect couch to 5982 to avoid default local instance connection conflict

--- AHC_app/setup_couchdb.sh
+++ AHC_app/setup_couchdb.sh
@@ -7,33 +7,37 @@ single_node=true
 [admins]
 ${COUCHDB_USER} = ${COUCHDB_PASSWORD}
 
+[chttpd]
+port = 5982
+bind_address = 0.0.0.0
+
 [httpd]
-;port = 5984
-;bind_address = 127.0.0.1
+port = 5982
+bind_address = 0.0.0.0
 
 EOF
 
 nohup bash -c "/docker-entrypoint.sh /opt/couchdb/bin/couchdb &"
 sleep 15
 
-#curl -X PUT http://127.0.0.1:5984/_users
-#curl -X PUT http://127.0.0.1:5984/_replicator
-#curl -X PUT http://127.0.0.1:5984/appendixes
+#curl -X PUT http://127.0.0.1:5982/_users
+#curl -X PUT http://127.0.0.1:5982/_replicator
+#curl -X PUT http://127.0.0.1:5982/appendixes
 
-response=$(curl -s -o /dev/null -w "%{http_code}" -X GET http://127.0.0.1:5984/_users)
+response=$(curl -s -o /dev/null -w "%{http_code}" -X GET http://127.0.0.1:5982/_users)
 if [ "$response" -ne "200" ]; then
-    curl -X PUT http://127.0.0.1:5984/_users
+    curl -X PUT http://127.0.0.1:5982/_users
     echo "Database _users was configured properly"
 fi
 
-response=$(curl -s -o /dev/null -w "%{http_code}" -X GET http://127.0.0.1:5984/_replicator)
+response=$(curl -s -o /dev/null -w "%{http_code}" -X GET http://127.0.0.1:5982/_replicator)
 if [ "$response" -ne "200" ]; then
-    curl -X PUT http://127.0.0.1:5984/_replicator
+    curl -X PUT http://127.0.0.1:5982/_replicator
     echo "Database _replicator was configured properly"
 fi
 
-response=$(curl -s -o /dev/null -w "%{http_code}" -X GET http://127.0.0.1:5984/appendixes)
+response=$(curl -s -o /dev/null -w "%{http_code}" -X GET http://127.0.0.1:5982/appendixes)
 if [ "$response" -ne "200" ]; then
-    curl -X PUT http://127.0.0.1:5984/appendixes
+    curl -X PUT http://127.0.0.1:5982/appendixes
     echo "Database appendixes was configured properly"
 fi
